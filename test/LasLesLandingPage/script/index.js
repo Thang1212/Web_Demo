b83c06f7db13bdb067d65e6fc87276df22739fb0
Thang1212 <wttttu1234@gmail.com>
2022-07-13 09:29:30 -0400

    test hero section

--- test/LasLesLandingPage/script/index.js
+++ test/LasLesLandingPage/script/index.js
@@ -3,11 +3,9 @@ const menu = document.querySelector('.menu');
 const activeClass = 'is-show';
 
 toggle.addEventListener('click', function () {
-    console.log(menu.classList);
     menu.classList.add(activeClass);
 })
 
 toggle.addEventListener('blur', function () {
-    console.log("Thang");
     menu.classList.remove(activeClass);
 })
